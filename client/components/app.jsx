8ba72e6668e251ab34cf8ecffc684ebc9eb866ef
Khoa Cao <khoacao0601@gmail.com>
2021-04-13 22:25:50 -0700

    Update app.jsx

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,7 +6,7 @@ import CartSummary from './cartSummary';
 import Checkout from './checkout';
 import Thankyou from './thankyou';
 import Footer from './footer';
-012345
+0123456789
 export default class App extends React.Component {
   constructor(props) {
     super(props);
